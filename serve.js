fddde4e0f663e2377eb80b5e474441e44f21208a
oleovini <williancardoso00004@gmail.com>
2023-10-19 17:06:34 -0400

    add descript serve.js

--- serve.js
+++ serve.js
@@ -10,5 +10,14 @@ app.post("/cript",  async function cript(req, res){
   const {password} = req.body;
   const newPassword = await bcript.hash(password, 11)
 
-  return res.send(password)
-})
+  return res.send(newPassword)
+});
+
+
+app.post("/descript",  async function descript(req, res){
+  const {password, newPassword} = req.body;
+  const response = await bcript.compare(password, newPassword)
+
+  return res.send(response)
+});
+
